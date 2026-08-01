53997c272d36669c0fd97e2d36ac856ed2ba9826
MilanMitra2210 <milanmitranda2020@gmail.com>
2026-08-02 04:02:20 +0530

    feat: add Jackson annotations to support snake_case for refreshToken field in RefreshTokenRequest

--- src/main/java/com/quillforge/api/user/dto/RefreshTokenRequest.java
+++ src/main/java/com/quillforge/api/user/dto/RefreshTokenRequest.java
@@ -1,5 +1,7 @@
 package com.quillforge.api.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,5 +11,7 @@ import lombok.Setter;
 public class RefreshTokenRequest {
 
     @NotBlank
+    @JsonProperty("refreshToken")
+    @JsonAlias("refresh_token")
     private String refreshToken;
 }
